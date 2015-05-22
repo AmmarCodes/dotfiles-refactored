2cc236d7ba01707966876666a03137fc7e3e7bfc
Ammar Alakkad <am.alakkad@gmail.com>
2015-05-22 19:17:24 +0300

    Remove toprc

--- install.sh
+++ install.sh
@@ -5,5 +5,4 @@ ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/gitignore ~/.gitignore
 ln -s ~/dotfiles/gitmessage ~/.gitmessage
-ln -s ~/dotfiles/toprc ~/.toprc
 ln -s ~/dotfiles/config.fish ~/.config/fish/config.fish
