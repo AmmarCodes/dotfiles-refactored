0171e87c1d849433fc79ea3bbe906d6b123b39b7
Ammar Alakkad <am.alakkad@gmail.com>
2015-05-24 23:32:02 +0300

    enhance aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -1,17 +1,15 @@
-# Misc
-alias ll 'ls -alh'
-
 # Vagrant
-alias vu 'vagrant up'
-alias vs 'vagrant ssh'
-alias vh 'vagrant halt'
-alias vss 'vagrant status'
+alias vu='vagrant up'
+alias vs='vagrant ssh'
+alias vh='vagrant halt'
+alias vss='vagrant status'
 
 ## Git
 alias gst="git status -s"
 alias ga="git add"
-alias gg "git log --oneline --graph --decorate --color"
-alias gd "git diff"
+alias gg="git log --oneline --graph --decorate --color"
+unalias gd
+alias gd="git diff"
 
 ## Zshmarks
 alias g="jump"
