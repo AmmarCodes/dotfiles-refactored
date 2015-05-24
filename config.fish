3321cf62dc4eb7b694fedaa920f8ddd2ba0ecbb0
Ammar Alakkad <am.alakkad@gmail.com>
2015-05-24 11:49:28 +0300

    fish: remove GREP_OPTIONS was causing warning

--- config.fish
+++ config.fish
@@ -1,8 +1,6 @@
 set -g -x fish_greeting ''
 set -g -x EDITOR vim
 
-set -gx GREP_OPTIONS '--color=auto'
-
 function prepend_to_path -d "Prepend the given dir to PATH if it exists and is not already in it"
     if test -d $argv[1]
         if not contains $argv[1] $PATH
